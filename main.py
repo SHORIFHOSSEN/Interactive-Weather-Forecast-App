423726185ebfefee7977478a89afef1a6d9eaa91
Md Shorif hossen <112204859+SHORIF69@users.noreply.github.com>
2025-10-07 13:21:02 +0600

    Day32: based on the user input we fact the data from the dataset

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help = "Select the number of forecasted days")
 option = st.selectbox("Select data to view:",
-                      ("Temperature", "Rainfall", "Humidity", "Wind Speed"))
+                      ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-def get_data(days):
-    dates = ["2022-25-10","2025-25-18","2022-27-10"]
-    temperature = [10,11,15]
-    temperature = [days * i for i in temperature]
-    return dates, temperature
-
+data = get_data(place,days,option)
 d, t = get_data(days)
 
 figure = px.line(x = d,y= t,labels={"x":"Date","y":"Temperature (c)"})
